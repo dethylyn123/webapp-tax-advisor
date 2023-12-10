d1fcad9fcbc3f5f46e327dc94e3e674ec35cc694
Dethylyn <dethylyn.b@gmail.com>
2023-12-11 00:05:21 +0800

    Added User APIs and Profile APIs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/user', [UserController::class, 'index']);
+Route::put('/user/image/{id}', [UserController::class, 'image'])->name('user.image');
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
+Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+//User Specific APIs = update of image based kong kinsa tong user nga ni log in
+Route::get('/profile/show', [ProfileController::class, 'show']);
+Route::put('/profile/image', [ProfileController::class, 'image'])->name('profile.image');

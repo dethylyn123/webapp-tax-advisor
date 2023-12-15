af5996fa860d898f6b1cdb25a620851e8c5ec477
Dethylyn <dethylyn.b@gmail.com>
2023-12-15 16:48:02 +0800

    Added logout API and create middleware groups

--- routes/api.php
+++ routes/api.php
@@ -25,51 +25,52 @@ use App\Http\Controllers\Api\LandClassificationController;
 //Public APIs
 Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Admin
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-// Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
-Route::put('/user/image/{id}', [UserController::class, 'image'])->name('user.image');
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
-Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::post('/user', [UserController::class, 'store'])->name('user.store');
+    Route::put('/user/image/{id}', [UserController::class, 'image'])->name('user.image');
+    Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
+    Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
-//User Specific APIs = update of image based kong kinsa tong user nga ni log in
-Route::get('/profile/show', [ProfileController::class, 'show']);
-Route::put('/profile/image', [ProfileController::class, 'image'])->name('profile.image');
+    //User Specific APIs = update of image based kong kinsa tong user nga ni log in
+    Route::get('/profile/show', [ProfileController::class, 'show']);
+    Route::put('/profile/image', [ProfileController::class, 'image'])->name('profile.image');
 
-Route::controller(PropertyOwnerController::class)->group(function () {
-    Route::get('/owner',               'index');
-    Route::get('/owner/{id}',          'show');
-    Route::post('/owner',              'store');
-    Route::put('/owner/{id}',          'update');
-    Route::delete('/owner/{id}',       'destroy');
-});
+    Route::controller(PropertyOwnerController::class)->group(function () {
+        Route::get('/owner',               'index');
+        Route::get('/owner/{id}',          'show');
+        Route::post('/owner',              'store');
+        Route::put('/owner/{id}',          'update');
+        Route::delete('/owner/{id}',       'destroy');
+    });
 
-Route::controller(PropertyController::class)->group(function () {
-    Route::get('/property',               'index');
-    Route::get('/property/{id}',          'show');
-    Route::post('/property',              'store');
-    Route::put('/property/{id}',          'update');
-    Route::delete('/property/{id}',       'destroy');
-});
+    Route::controller(PropertyController::class)->group(function () {
+        Route::get('/property',               'index');
+        Route::get('/property/{id}',          'show');
+        Route::post('/property',              'store');
+        Route::put('/property/{id}',          'update');
+        Route::delete('/property/{id}',       'destroy');
+    });
 
-Route::controller(LandClassificationController::class)->group(function () {
-    Route::get('/classification',               'index');
-    Route::get('/classification/{id}',          'show');
-    Route::post('/classification',              'store');
-    Route::put('/classification/{id}',          'update');
-    Route::delete('/classification/{id}',       'destroy');
-});
+    Route::controller(LandClassificationController::class)->group(function () {
+        Route::get('/classification',               'index');
+        Route::get('/classification/{id}',          'show');
+        Route::post('/classification',              'store');
+        Route::put('/classification/{id}',          'update');
+        Route::delete('/classification/{id}',       'destroy');
+    });
 
-Route::controller(RealPropertyTaxController::class)->group(function () {
-    Route::get('/tax',               'index');
-    Route::get('/tax/{id}',          'show');
-    Route::post('/tax',              'store');
-    Route::put('/tax/{id}',          'update');
-    Route::delete('/tax/{id}',       'destroy');
+    Route::controller(RealPropertyTaxController::class)->group(function () {
+        Route::get('/tax',               'index');
+        Route::get('/tax/{id}',          'show');
+        Route::post('/tax',              'store');
+        Route::put('/tax/{id}',          'update');
+        Route::delete('/tax/{id}',       'destroy');
+    });
 });
